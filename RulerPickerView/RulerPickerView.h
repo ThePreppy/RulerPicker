0b30dc460bc773282d3cdf87fc00cf097fbb099d
Alexander Gerega <aleksandr@MacBook-Pro-Aleksandr.local>
2020-09-20 19:18:42 +0300

    framework target implementation

---
+++ RulerPickerView/RulerPickerView.h
@@ -0,0 +1,19 @@
+//
+//  RulerPickerView.h
+//  RulerPickerView
+//
+//  Created by Александр on 20.09.2020.
+//  Copyright © 2020 Alexander. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RulerPickerView.
+FOUNDATION_EXPORT double RulerPickerViewVersionNumber;
+
+//! Project version string for RulerPickerView.
+FOUNDATION_EXPORT const unsigned char RulerPickerViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RulerPickerView/PublicHeader.h>
+
+
